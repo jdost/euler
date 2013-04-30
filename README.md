247b89a62e3baa52c3f8ace02efe320e95cbc52e
Jeff <jostendorf@gmail.com>
2013-04-30 13:43:39 -0500

    Various additions from cleaning up repo
    Added a README that has the problem text from the site, made the example in the
    problem a txt file to run.  The solution code has been heavily rewritten, I
    moved most of the code into OO format, added some printing functionality.  Right
    now the guessing system has a lot of issues, need to add some better logic
    optimizations (common NumSets on exclusives)

---
+++ README.md
@@ -0,0 +1,10 @@
+# Project Euler
+
+This is a collection of problems (and attempted solutions) at various problems from
+the [Project Euler](http://projecteuler.net).  I will (hopefully) add entries in 
+the Makefile to run basic unit tests against the attempted code and also to run the 
+actual code.  A lot of the format will be figured out eventually.
+
+This started from a hack night where we were to compete to see which team could 
+solve the most problems (based on problem # for value).  Go "Ferris Euler's Day 
+Off"!
--- sudoku/solution.py
+++ sudoku/solution.py
@@ -1,209 +1,383 @@
-import copy
 PREFIX = "Grid"
 
 
+class CollisionError(BaseException):
+    pass
+
+
+class NoPossibilityError(BaseException):
+    pass
+
+
+class NumSet:
+    ''' class NumSet:
+    Wrapper class for a "number set" which is defined as either a 3x3 square,
+    a row, or a column in the Sudoku board.  This means that the nodes that
+    belong to it should each have one of each number between 1 and 9.
+    '''
+    def __init__(self):
+        self.nodes = []
+        self.answers = set()
+
+    def exclusives(self):
+        ''' (public) exclusives:
+        Loops through the unanswered nodes for this NumSet, tracking the number
+        of occurances of each possibility, if any possibility has only one
+        occurance, the Node with that possibility must be the one with that
+        answer.
+        '''
+        possibility_count = {}
+        for i in range(1, 10):
+            if i not in self.answers:
+                possibility_count[i] = 0
+
+        for node in self.nodes:
+            if node.is_solved():
+                continue
+            for possibility in node.possibilities:
+                possibility_count[possibility] += 1
+
+        found = 0
+        for (number, count) in possibility_count.items():
+            if count == 1:
+                for node in self.nodes:
+                    if number in node.possibilities:
+                        found += 1
+                        node.set_answer(number)
+                        break
+
+        return found
+
+
+class Node:
+    ''' class Node:
+    Represents a single number square on the board, has a single answer, it
+    keeps references to the 3x3 square, row, and column NumSet it belongs to.
+    It either has an `answer` which is defined as the proper solution for the
+    Node or a set of `possibilities` that is a group of potential solutions for
+    the square.
+    '''
+    def __init__(self, answer, row, column):
+        self.answer = answer
+        self.row_index = row
+        self.column_index = column
+        self.square_index = (column / 3 + 3 * (row / 3))
+        self.numsets = []
+        self.possibilities = set()
+
+        if answer == 0:
+            self.possibilities = set(range(1, 10))
+
+    def attach_numset(self, numset):
+        ''' (public) attach_numset:
+        Attaches a NumSet
+        '''
+        self.numsets.append(numset)
+        if self.is_solved:
+            numset.answers.add(self.answer)
+
+    def is_solved(self):
+        ''' (public) is_solved
+        Returns True/False whether this is a solved node
+        '''
+        return self.answer != 0
+
+    def set_answer(self, answer):
+        ''' (public) set_answer:
+        Sets the answer to the Node, should clear the possibilities and should
+        add its answer to the answer set in each of it's NumSets
+        '''
+        self.answer = answer
+        self.possibilities = set()
+        for numset in self.numsets:
+            if self.answer in numset.answers:
+                raise CollisionError(self.answer)
+            numset.answers.add(self.answer)
+
+    def clip(self):
+        ''' (public) clip:
+        Goes through the already answered values for each of this Node's
+        NumSets and eliminates those from its possibilities pool.  If the set
+        is only 1 item, this should be the answer for the square.  This then
+        gets set as the answer.
+
+        Returns whether this clip call solved the Node and the number of
+        possibilities clipped
+        '''
+        orig_len = len(self.possibilities)
+        for numset in self.numsets:
+            self.possibilities = self.possibilities - numset.answers
+
+        if len(self.possibilities) == 1:
+            self.set_answer(self.possibilities.pop())
+        elif len(self.possibilities) == 0:
+            raise NoPossibilityError
+
+        return self.is_solved(), orig_len - len(self.possibilities)
+
+    def __str__(self, verbose=False):
+        ''' (magic) __str__:
+        '''
+        if not verbose:
+            return str(self.answer) if self.is_solved() else " "
+        else:
+            return {
+                "answer": self.answer,
+                "possibilities": self.possibilities
+            }.__str__()
+
+
+class Board:
+    ''' class Board:
+    Large wrapper class to encapsulate the basics of a board.  Using an object
+    approach to gather the various operators on the board into a common
+    namespace.
+    '''
+    def __init__(self):
+        self.squares = []
+        self.columns = []
+        self.rows = []
+        self.nodes = []
+        self.numsets = []
+        self.solved = 0
+
+        for i in range(9):
+            self.squares.append(self.__make_numset())
+            self.columns.append(self.__make_numset())
+            self.rows.append(self.__make_numset())
+
+    def __make_numset(self):
+        ''' (private) __make_numset:
+        Simple function to generate a NumSet object, wrapped as a private
+        method in case of possible future actions that may need to be done.
+        '''
+        numset = NumSet()
+        self.numsets.append(numset)
+        return numset
+
+    def load_row(self, row_text, row):
+        ''' (public) load_row:
+        Takes a string of text that represents a row of the board, creating
+        the nodes and referencing the nodes to their respective numsets (row,
+        column, and square).
+        '''
+        column = 0
+        for num in row_text:
+            if num < '0' or num > '9':
+                continue
+            elif num > '0':
+                self.solved += 1
+            self.build_node(int(num), row, column)
+            column += 1
+
+    def build_node(self, val, row, col):
+        ''' (public) build_node:
+        Takes a value of a node, a row index and a column index and creates a
+        solution node at that location, with references to the respective
+        numsets.
+        '''
+        node = Node(val, row, col)
+        node.attach_numset(self.squares[node.square_index])
+        node.attach_numset(self.rows[row])
+        node.attach_numset(self.columns[col])
+        self.nodes.append(node)
+
+        return node
+
+    def clip(self):
+        ''' (public) clip:
+        Method that loops over each node and trims or clips down the
+        possibility list for each node (the possibility list is a list of all
+        of the potential values the node can have).  Returns the number of
+        nodes that had possibilities that could be clipped.
+        '''
+        solved_orig = self.solved
+        for node in self.nodes:
+            if node.is_solved():
+                continue
+            solved, changed = node.clip()
+            if solved:
+                self.solved += 1
+
+        return self.solved - solved_orig
+
+    def full_clip(self):
+        ''' (public) full_clip:
+        Runs the `clip` optimization method until the node possiblities sets
+        can no longer be optimized/clipped.  This means finding every Node
+        that, by process of elimination, only has one possibility and answering
+        it, then looping through again to update other, related, nodes against
+        this new elimination.
+        '''
+        clip = 1
+        while clip:
+            clip = self.clip()
+
+    def exclusives(self):
+        ''' (public) exclusives:
+        Runs the `exclusives` optimization method, then `clip`s the Nodes if
+        any optimizations were found.  The optimization runs on the idea that
+        if a Node has a possibility that is unique to the unanswered Nodes in
+        a NumSet, it, by process of elimination, must be the answer (otherwise
+        the NumSet would not ever have that as an answer).  A `full_clip` is
+        run after if an exclusive was found to optimize the possibilities for
+        all of the other squares.
+        '''
+        exc = 0
+        for numset in self.numsets:
+            exc += numset.exclusives()
+        if exc:
+            self.full_clip()
+
+        return exc
+
+    def clone(self):
+        ''' (public) clone:
+        Performs an optimized deep copy of the Board.  It needs to be optimized
+        because the Nodes hold circular references with their NumSets.
+        '''
+        clone = Board()
+        clone.solved = self.solved
+        for node in self.nodes:
+            node_ = clone.build_node(node.answer, node.row_index,
+                                     node.column_index)
+            node_.possibilities = node.possibilities.copy()
+
+        return clone
+
+    def __str__(self):
+        ''' (magic) __str__:
+        Prints a representation of the Board, useful for debugging purposes.
+        '''
+        output = ""
+        index = 0
+        for i in range(9):
+            if i > 0 and i % 3 == 0:
+                output += "------+-------+------\n"
+            output += ""
+            for j in range(9):
+                if j > 0 and j % 3 == 0:
+                    output += "| "
+                output += str(self.nodes[index]) + " "
+                index += 1
+
+            output += "\n"
+
+        return output
+
+
 def load_file(filename):
-    ''' loads the puzzles from the file
+    ''' load_file:
+    The given filename is read in against the standard used in the puzzle file.
+    Looping over each board in the file, the board is loaded and then attempted
+    to be solved.
+
+    file format example:
+        Grid X
+        01000200003
+        90000070000
+        etc...
     '''
     f = open(filename, 'r')
-    line = f.readline()
+    line = "~~"
+    solved_cnt = 0
     while line:
-        if line.startswith(PREFIX):
-            load_board(f)
         line = f.readline()
+        if line.startswith(PREFIX):
+            cnt = load_board(f)
+            if cnt == 81:
+                print "solved"
+                solved_cnt += 1
+            else:
+                print "unsolved - " + str(cnt)
+
+    print "solved " + str(solved_cnt) + " puzzles"
 
 
 def load_board(fp):
     ''' takes the file pointer and loads in a board set
     '''
-    board = {
-        "squares": [],
-        "columns": [],
-        "rows": [],
-        "nodes": []
-    }
-    for i in range(9):
-        board['rows'].append({
-            'nodes': [],
-            'answers': []
-        })
-        board['columns'].append({
-            'nodes': [],
-            'answers': []
-        })
-        board['squares'].append({
-            'nodes': [],
-            'answers': []
-        })
+    board = Board()
     for i in range(9):
         line = fp.readline()
-        j = 0
-        for num in line:
-            if num >= '0' and num <= '9':
-                board['nodes'].append(build_node(int(num), i, j, board))
-            j += 1
+        board.load_row(line, i)
 
-    solve_board(board)
+    return solve(board)
 
 
-def build_node(number, row, column, board):
-    ''' ???
+def solve(board):
+    board.full_clip()
+    found = 1
+    while found:
+        found = board.exclusives()
+
+    while board.solved < 81:
+        poss_total = 0
+        for node in board.nodes:
+            poss_total += len(node.possibilities)
+        break
+        board_ = board.clone()
+        try:
+            res, value = simulate_guess(board_)
+        except:
+            break
+
+        if not res:
+            print value
+            board.nodes[value[0]].possibilities.remove(value[1])
+        else:
+            board = value
+
+        poss_total_ = 0
+        for node in board.nodes:
+            poss_total_ += len(node.possibilities)
+
+        if poss_total_ == 0:
+            break
+
+        print poss_total, poss_total_, board.solved
+
+    print board
+    return board.solved
+
+
+def simulate_guess(board):
+    ''' simulate_guess:
+    Finds an unsolved node with the lowest number of possibilities, picks the
+    first one and attempts to solve with that as an answer, if the result is
+    unsolvable, returns False, otherwise returns a solved board
     '''
-    node = {
-        "answer": number,
-        "row": board['rows'][row],
-        "column": board['columns'][column],
-        "square": board['squares'][(column / 3 + 3 * (row / 3))]
-    }
+    guess_node = None
+    node_index = 0
+    i = 0
+    possibility_count = 10
+    for node in board.nodes:
+        if not node.is_solved() \
+                and len(node.possibilities) < possibility_count:
+            guess_node = node
+            node_index = i
+            possibility_count = len(node.possibilities)
+            if possibility_count == 0:
+                raise NoPossibilityError
+        i += 1
 
-    if number > 0:
-        node['possibilities'] = []
-        node['column']['answers'].append(number)
-        node['row']['answers'].append(number)
-        node['square']['answers'].append(number)
-    else:
-        node['possibilities'] = range(1, 10)
+    if not guess_node:
+        return True, board
 
-    node['column']['nodes'].append(node)
-    node['row']['nodes'].append(node)
-    node['square']['nodes'].append(node)
+    guess = guess_node.possibilities.pop()
+    guess_node.set_answer(guess)
 
-    return node
+    try:
+        board.full_clip()
+        found = 1
+        while found:
+            found = board.exclusives()
+    except:
+        return False, (node_index, guess)
 
+    return True, board
 
-def solve_board(board):
-    ''' loops through nodes trying to eliminate possiblities
-    '''
-    answered = -1
-    board, answered = logic_board(board)
-    while answered < 81:
-        i = 0
-        board_ = copy.copy(board)
-        for node in board_['nodes']:
-            if node['answer'] == 0:
-                node['answer'] = node['possibilities'][0]
-                try:
-                    board, answered = logic_board(board_)
-                except BaseException:
-                    del board['nodes'][i]['possibilities'][0]
-                break
-            i += 1
-
-    if answered == 81:
-        print "solved"
-    else:
-        print "unsolved - " + str(answered)
-
-
-def logic_board(board):
-    clips = 1
-    solved = 1
-    while clips > 0 and solved > 0:
-        board, clips, answered = clip_possibilities(board)
-        while clips > 0:
-            board, clips, answered = clip_possibilities(board)
-        board, solved = find_exclusives(board)
-        while solved > 0:
-            board, solved = find_exclusives(board)
-
-    return board, answered
-
-
-def clip_possibilities(board):
-    clips = 0
-    answered = 0
-    for node in board['nodes']:
-        if node['answer'] > 0:
-            answered += 1
-            continue
-
-        for answer in node['column']['answers']:
-            if answer in node['possibilities']:
-                clips += 1
-                node['possibilities'].remove(answer)
-        for answer in node['row']['answers']:
-            if answer in node['possibilities']:
-                clips += 1
-                node['possibilities'].remove(answer)
-        for answer in node['square']['answers']:
-            if answer in node['possibilities']:
-                clips += 1
-                node['possibilities'].remove(answer)
-
-        if len(node['possibilities']) == 1:
-            node['answer'] = node['possibilities'][0]
-            if node['answer'] in node['column']['answers']:
-                raise
-            if node['answer'] in node['row']['answers']:
-                raise
-            if node['answer'] in node['square']['answers']:
-                raise
-            node['column']['answers'].append(node['answer'])
-            node['row']['answers'].append(node['answer'])
-            node['square']['answers'].append(node['answer'])
-            node['possibilities'] = []
-
-    return board, clips, answered
-
-
-def find_exclusives(board):
-    solved = 0
-    index = 0
-    for row in board['rows']:
-        board['rows'][index], s = find_exc(row)
-        index += 1
-    solved += s
-
-    index = 0
-    for column in board['columns']:
-        board['columns'][index], s = find_exc(column)
-        index += 1
-    solved += s
-
-    index = 0
-    for square in board['squares']:
-        board['squares'][index], s = find_exc(square)
-        index += 1
-    solved += s
-
-    return board, solved
-
-
-def find_exc(numset):
-    poss_count = {}
-    for i in range(1, 10):
-        poss_count[i] = 0
-
-    for node in numset['nodes']:
-        if node['answer'] > 0:
-            continue
-        for poss in node['possibilities']:
-            poss_count[poss] += 1
-
-    solved = 0
-    for (num, cnt) in poss_count.items():
-        if cnt == 1:
-            solved += 1
-            for node in numset['nodes']:
-                if node['answer'] > 0:
-                    continue
-                if num in node['possibilities']:
-                    if node['answer'] in node['column']['answers']:
-                        print "column", node['answer']
-                        raise BaseException
-                    if node['answer'] in node['row']['answers']:
-                        print "row", node['answer']
-                        raise BaseException
-                    if node['answer'] in node['square']['answers']:
-                        print "square", node['answer']
-                        raise BaseException
-                    node['answer'] = num
-                    node['column']['answers'].append(node['answer'])
-                    node['row']['answers'].append(node['answer'])
-                    node['square']['answers'].append(node['answer'])
-                    node['possibilities'] = []
-                    break
-
-    return numset, solved
-
-load_file("sudoku.txt")
+if __name__ == "__main__":
+    import sys
+    filename = sys.argv[1] if len(sys.argv) > 1 else "sudoku.txt"
+    load_file(filename)
